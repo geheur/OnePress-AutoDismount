0abcef5fd686c7df2348fedf40129d19be52f4ad
geheur <dasistkeinnamen@gmail.com>
2019-09-13 03:38:03 +0100

    Removed all EzDismount code except for the name of the toc file and folder

--- TwoPress.lua
+++ TwoPress.lua
@@ -19,98 +19,31 @@ local MountErrorMessages = {
 	ERR_ATTACK_MOUNTED,
 }
 
-function EzDismount_chkerror(errorId, errorMessage)
-   -- Stand up if you are trying to do something while sitting
-   if (errorMessage == EzDSitErr )  then
-      if (EzDismount_Config[EzDPlayer]["Stand"] == "ON") then
-         DoEmote("stand")
-			UIErrorsFrame:Clear()
-      end
-      return;
-   end
-   
-    -- Flightpath Dismount enabled
-   if (errorMessage == "**TAXI**" ) then
-      if (EzDismount_Config[EzDPlayer]["Dismount"] == "ON") then
-         EzD_dismount();
-      end
-      return;
-   end
-
-   -- Auctioneer Dismount enabled
-   if (errorMessage == "**AUCTION**" ) then
-      if (EzDismount_Config[EzDPlayer]["Auction"] == "ON") then
-         EzD_dismount();
-      end
-      return;
-   end
-
-   -- Mount Error
-   if ( not UnitOnTaxi("player") ) then
-      for _, value in pairs(EzDMountErr.Error) do
-          if (errorMessage == value ) then
-             if (EzDismount_Config[EzDPlayer]["Dismount"] == "ON") then
-                EzD_dismount();
-             end
-             return;
-          end
-      end
-   end
-
-   -- Shapeshift Error
-   if ( not UnitOnTaxi("player") ) then
-      for _, value in pairs(EzDShiftErr.Error) do
-          if (errorMessage == value) then
-             EzD_unshift()
+local f = CreateFrame("FRAME")
+f:RegisterEvent("UI_ERROR_MESSAGE")
+f:SetScript("OnEvent", function(self, event, errorId, errorMessage)
+	if errorMessage == SitErrorMessage then
+		DoEmote("stand")
+		UIErrorsFrame:Clear()
+	elseif errorMessage == "**TAXI**" then
+		Dismount()
+	elseif errorMessage == "**AUCTION**" then
+		Dismount()
+	elseif not UnitOnTaxi("player") then
+		for _,message in ipairs(MountErrorMessages) do
+			 if errorMessage == message then
+				Dismount()
+				UIErrorsFrame:Clear()
+				return
+			 end
+		end
+		for _,message in ipairs(ShapeshiftErrorMessages) do
+			 if errorMessage == value then
+				 CancelShapeshiftForm()
 				 UIErrorsFrame:Clear()
-          end
-      end
-   end
-
-end
-
--------------------------
--- Cancel Shapeshift Buff
--------------------------
-function EzD_unshift()
-
-	CancelShapeshiftForm()
-	if true then return end
-
-   local shifttable = nil;
-
-   -- Set Texture Table based on Class
-   if (EzDClass == "Druid") then
-      if ( EzDismount_Config[EzDPlayer]["Druid"] == "ON" ) then
-         shifttable = EzDMountText.Druid;
-      end
-   elseif (EzDClass == "Shaman") then
-      if ( EzDismount_Config[EzDPlayer]["Wolf"] == "ON" ) then
-         shifttable = EzDMountText.Shaman;
-      end
-   elseif (EzDClass == "Priest") then
-      if ( EzDismount_Config[EzDPlayer]["Shadowform"] == "ON" ) then
-         shifttable = EzDMountText.Priest;
-      end
-   end
-
-   -- Only Continue if we have a table to work with
-   if shifttable ~= nil then
-      -- Loop thru the 16 max player buffs
-      for i=0,16,1 do
-
-         -- Skip over excluded textures
-         if not EzD_shiftexclude(i) then
-
-            -- Look for shapeshift buff texture
-            if EzD_checktext(i, shifttable) then
-               CancelPlayerBuff(i);
-            end
-
-         end
-
-      end
-   end
-
-end
+				 return
+			 end
+		end
+	end
+end)
 
